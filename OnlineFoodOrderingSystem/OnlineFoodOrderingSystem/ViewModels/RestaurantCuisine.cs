42742ffdc4360c60183aaf865d8b88c7913899dc
mohamad-alhawmi <moda.tnt.edu@outlook.com>
2021-08-20 15:43:40 +0300

    Added view model to join restaurant and cuisine table.

---
+++ OnlineFoodOrderingSystem/OnlineFoodOrderingSystem/ViewModels/RestaurantCuisine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OnlineFoodOrderingSystem.Models;
+
+
+namespace OnlineFoodOrderingSystem.ViewModels
+{
+    public class RestaurantCuisine
+    {
+        public int restaurantId { get; set; }
+        public string restaurantName { get; set; }
+        public string restaurantTown { get; set; }
+        public byte[] restaurantLogo { get; set; }
+        public string cuisineName { get; set; }
+    }
+}
