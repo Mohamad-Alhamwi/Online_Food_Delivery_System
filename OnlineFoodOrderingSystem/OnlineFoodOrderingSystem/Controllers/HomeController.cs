e618a494a6689c0a2b0fde17104fa60baef0103f
mohamad-alhawmi <moda.tnt.edu@outlook.com>
2021-08-20 15:37:57 +0300

    Wrote LINQ query to retrieve resturants with cuisine names.

--- OnlineFoodOrderingSystem/OnlineFoodOrderingSystem/Controllers/HomeController.cs
+++ OnlineFoodOrderingSystem/OnlineFoodOrderingSystem/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OnlineFoodOrderingSystem.Models;
+using OnlineFoodOrderingSystem.ViewModels;
 
 namespace OnlineFoodOrderingSystem.Controllers
 {
@@ -11,10 +13,49 @@ namespace OnlineFoodOrderingSystem.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            Model1 m = new Model1();
+
+            List<Cuisine> cuisines = m.Cuisine.ToList();
+            List<Restaurant> restaurants = m.Restaurant.ToList();
+
+            // LINQ Queri to retrieve restaurants with cuisine names.
+            var restaurantsLinqQueri = restaurants.Join(cuisines,
+                                          r => r.cuisine_id,
+                                          c => c.id,
+                                          (restaurant, cuisine) => new RestaurantCuisine    
+                                          {
+                                              restaurantId = restaurant.id,
+                                              restaurantName = restaurant.restaurant_name,
+                                              restaurantTown = restaurant.town,
+                                              restaurantLogo = restaurant.logo,
+                                              cuisineName = cuisine.cuisine_name,
+                                          }).ToList();
+
             ViewBag.SubTitle = "Home";
             ViewBag.Path = "/Home/Index/";
+            ViewBag.Cuisines = cuisines;
+            ViewBag.Restaurants = restaurantsLinqQueri;
 
             return View();
         }
+
+        /*[HttpGet]
+        public ActionResult DisplayMenusByCuisines(Cuisine cuisine)
+        {
+            Model1 m = new Model1();
+
+            // Return the restaurants  belonging to that cuisine.
+            List<Restaurant> restaurants = m.Restaurant.Where(x => x.cuisine_id == cuisine.id).ToList();
+
+            // Return the products.
+            List< List<Product> > products = new List<List<Product>>();
+
+            for (int counter = 0; counter < restaurants.Count; ++ counter)
+            {
+                products.Add(m.Product.Where(x => x.id_restaurant == restaurants[counter].id).ToList());
+            }
+
+            return View(products);
+        }*/
     }
 }
