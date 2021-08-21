08e91d5f066d7c00351657dacdc4876ac1689f45
mohamad-alhawmi <moda.tnt.edu@outlook.com>
2021-08-21 14:02:04 +0300

    Added Login functionality.

---
+++ OnlineFoodOrderingSystem/OnlineFoodOrderingSystem/Controllers/SecurityController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using OnlineFoodOrderingSystem.Models;
+
+
+namespace OnlineFoodOrderingSystem.Controllers
+{
+    public class SecurityController : Controller
+    {
+        // GET: Security
+        
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User_ user)
+        {
+            Model1 m = new Model1();
+
+            User_ u = m.User_.FirstOrDefault(x => x.email == user.email && x.pwd == user.pwd);
+
+            if (u != null)
+            {
+                FormsAuthentication.SetAuthCookie(u.email, false);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Msg = "The email or password is incorrect!";
+                return View();
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Home");
+        }
+
+    }
+}
