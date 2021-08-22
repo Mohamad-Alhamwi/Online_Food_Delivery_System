dbbdc1f9dd845cf748ecb43728fb1fa21dc0bccd
mohamad-alhawmi <moda.tnt.edu@outlook.com>
2021-08-22 23:08:01 +0300

    Added views, controllers and styles to listing the products.

--- OnlineFoodOrderingSystem/OnlineFoodOrderingSystem/Controllers/HomeController.cs
+++ OnlineFoodOrderingSystem/OnlineFoodOrderingSystem/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using OnlineFoodOrderingSystem.ViewModels;
 
 namespace OnlineFoodOrderingSystem.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         // GET: Home
@@ -39,23 +40,55 @@ namespace OnlineFoodOrderingSystem.Controllers
             return View();
         }
 
-        /*[HttpGet]
+        [HttpGet]
         public ActionResult DisplayMenusByCuisines(Cuisine cuisine)
         {
             Model1 m = new Model1();
 
-            // Return the restaurants  belonging to that cuisine.
-            List<Restaurant> restaurants = m.Restaurant.Where(x => x.cuisine_id == cuisine.id).ToList();
+            List<Product> products = m.Product.ToList();
+            List<Restaurant> restaurants = m.Restaurant.ToList();
 
-            // Return the products.
-            List< List<Product> > products = new List<List<Product>>();
+            // LINQ Queri to retrieve products with details.
+            var ProductsLinqQueri = products.Join(restaurants,
+                                          p => p.id_restaurant,
+                                          r => r.id,
+                                          (product, restaurant) => new ProductDetails
+                                          {
+                                                cuisineId = restaurant.cuisine_id,
+                                                restaurantId = product.id_restaurant,
+                                                categoryId = product.category_id,
+                                                productId = product.id,
+                                                restaurantName = restaurant.restaurant_name,
+                                                productName = product.product_name,
+                                                productDescription = product.product_description,
+                                                productPrice = product.price,
+                                                productWeight = product.product_weight,
+                                                productImage = product.product_image
+                                          }).ToList();
 
-            for (int counter = 0; counter < restaurants.Count; ++ counter)
+            // Getting only the products of restaurants of that cuisine.
+            List<ProductDetails> product_details = new List<ProductDetails>();
+
+            for (int counter = 0; counter < ProductsLinqQueri.Count; ++ counter)
             {
-                products.Add(m.Product.Where(x => x.id_restaurant == restaurants[counter].id).ToList());
+                if(ProductsLinqQueri[counter].cuisineId == cuisine.id)
+                {
+                    product_details.Add(ProductsLinqQueri[counter]);
+                }
             }
 
+            ViewBag.ProductsDetails = product_details;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult DisplayMenusByRestaurants(Restaurant restaurant)
+        {
+            Model1 m = new Model1();
+
+            List<Product> products = m.Product.Where(x => x.id_restaurant == restaurant.id).ToList();
+
             return View(products);
-        }*/
+        }
     }
 }
---
+++ OnlineFoodOrderingSystem/OnlineFoodOrderingSystem/ViewModels/ProductDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OnlineFoodOrderingSystem.Models;
+
+namespace OnlineFoodOrderingSystem.ViewModels
+{
+    public class ProductDetails
+    {
+        public int cuisineId { get; set; }
+        public int restaurantId { get; set; }
+        public int categoryId { get; set; }
+        public int productId { get; set; }
+        public string restaurantName { get; set; }
+        public string categoryName { get; set; }
+        public string productName { get; set; }
+        public string productDescription { get; set; }
+        public decimal productPrice { get; set; }
+        public decimal productWeight { get; set; }
+        public byte[] productImage { get; set; }
+    }
+}
