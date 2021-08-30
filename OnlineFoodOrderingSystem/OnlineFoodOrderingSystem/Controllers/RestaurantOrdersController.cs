601f16903a52a9cfd09ee97425e0e0e1572f677a
mohamad-alhawmi <moda.tnt.edu@outlook.com>
2021-08-30 13:33:29 +0300

    Added controller and view for RestaurantOrders.

---
+++ OnlineFoodOrderingSystem/OnlineFoodOrderingSystem/Controllers/RestaurantOrdersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineFoodOrderingSystem.Controllers
+{
+    public class RestaurantOrdersController : Controller
+    {
+        // GET: RestaurantOrders
+        public ActionResult Index()
+        {
+            ViewBag.SubTitle = "Restaurant Orders";
+
+            return View();
+        }
+    }
+}
